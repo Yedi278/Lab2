4c6399fa12ba27c3624dd7ae8ddb49bc8869c759
Yehan <y.edirisinghemudi@campus.unimib.it>
2024-05-25 12:45:53 +0200

    .

--- Circuiti_2/funcs.py
+++ Circuiti_2/funcs.py
@@ -1,6 +1,5 @@
 import os
 from IPython.display import Latex
-import sympy as sp
 import numpy as np
 import pandas as pd
 
@@ -23,32 +22,32 @@ def get_data(path:str):
     try:
         name = find_file(path,'CH1')
         if name != None:
-            first_channel = pd.read_csv(path+name).iloc[:,3].to_numpy(np.float128) , pd.read_csv(path+name).iloc[:,4].to_numpy(np.float128)
+            first_channel = pd.read_csv(path+name).iloc[:,3].to_numpy() , pd.read_csv(path+name).iloc[:,4].to_numpy()
         else:
             first_channel = None
-    except:
+    except Exception as e:
         first_channel = None
-        print('Error loading first channel')
+        print('Error loading first channel: ',e)
 
     try:
         name = find_file(path,'CH2')
         if name != None:
-            second_channel = pd.read_csv(path+name).iloc[:,3].to_numpy(np.float128) , pd.read_csv(path+name).iloc[:,4].to_numpy(np.float128)
+            second_channel = pd.read_csv(path+name).iloc[:,3].to_numpy() , pd.read_csv(path+name).iloc[:,4].to_numpy()
         else:
             second_channel = None
-    except:
+    except Exception as e:
         second_channel = None
-        print('Error loading second channel')
+        print('Error loading second channel: ',e)
         
     try:
         name = find_file(path,'MTH')
         if name != None:
-            third_channel = pd.read_csv(path+name).iloc[:,3].to_numpy(np.float128) , pd.read_csv(path+name).iloc[:,4].to_numpy(np.float128)
+            third_channel = pd.read_csv(path+name).iloc[:,3].to_numpy() , pd.read_csv(path+name).iloc[:,4].to_numpy()
         else:
             third_channel = first_channel[0], None
-    except:
+    except Exception as e:
         third_channel = first_channel[0], None
-        print('Error loading third channel')
+        print('Error loading third channel: ',e)
     
 
     return first_channel, second_channel, third_channel
