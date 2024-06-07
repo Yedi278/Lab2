57719ae54ad3178ccda5343b202ddb0b1b670baf
y.edirisinghe <y.edirisinghemudi@campus.unimib.it>
2024-06-07 19:05:29 +0200

    rlc new

--- Circuiti_3/funcs.py
+++ Circuiti_3/funcs.py
@@ -69,11 +69,11 @@ def find_min(data,init=0, prec=0.001):
         i += 1
     return data[i], i
 
-def find_zero(data,init=0, prec=0.001):
+def find_zero(data, _init=0, prec=0.001):
 
-    i=init
+    i=_init
 
-    if data[init] > 0.+prec:
+    if data[_init] > 0.+prec:
         while data[i] > 0+prec:
             i += 1
         return data[i], i
@@ -89,3 +89,33 @@ def chi_2_fit(x,y,yerr,model,kwargs):
     m.fixed[*(kwargs.keys())] = True, True
     m.migrad()
     return m.fval, m.ndof
+
+def analize(path, init=0, verbose=False)->tuple:
+    '''
+    `path`: path to the data file
+    `init`: initial guess for the maximum
+    `verbose`: if True, returns all the data, if False, returns only the relevant data
+    `return`: tuple with the relevant data as follows:
+        V_CH1_SGN, V_MTH_SGN, dt_CH1_SGN, dt_MTH_SGN
+    '''
+    CH1, SGN, MTH = get_data(path)
+    N = init
+
+    max_SGN, i_max_SGN = find_max(SGN[1], N, prec=0.00001)
+    max_CH1, i_max_CH1 = find_max(CH1[1], N, prec=0.00001)
+    max_MTH, i_max_MTH = find_max(MTH[1], N, prec=0.00001)
+    z, i_zero_SGN = find_zero(SGN[1], N, prec=0.00001)
+    z, i_zero_CH1 = find_zero(CH1[1],  _init=i_zero_SGN, prec=0.00001)
+    z, i_zero_MTH = find_zero(MTH[1],  _init=i_zero_SGN, prec=0.00001)
+
+    V_CH1_SGN = max_CH1/max_SGN
+    V_MTH_SGN = max_MTH/max_SGN
+
+    dt_CH1_SGN = np.abs(CH1[0][i_zero_CH1] - SGN[0][i_zero_SGN])
+    dt_MTH_SGN = np.abs(MTH[0][i_zero_MTH] - SGN[0][i_zero_SGN])
+
+    if verbose:
+        return CH1,SGN,MTH, V_CH1_SGN, V_MTH_SGN, dt_CH1_SGN, dt_MTH_SGN, max_CH1, max_SGN, max_MTH, i_max_CH1, i_max_SGN, i_max_MTH, i_zero_CH1, i_zero_SGN, i_zero_MTH
+    
+    else:
+        return V_CH1_SGN, V_MTH_SGN, dt_CH1_SGN, dt_MTH_SGN
